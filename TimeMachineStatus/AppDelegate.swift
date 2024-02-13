4078f934147ee86c3fe6764968f6664a6bee8a6c
Lukas Pistrol <lukas@pistrol.com>
2024-02-13 11:29:09 +0100

    fix lint error

--- TimeMachineStatus/AppDelegate.swift
+++ TimeMachineStatus/AppDelegate.swift
@@ -156,6 +156,7 @@ class DetachedWindowController: NSWindowController {
         window.standardWindowButton(.zoomButton)?.isHidden = true
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
